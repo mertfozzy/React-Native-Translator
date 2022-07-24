bcb190d39691ed440c71abcd29d15aa143bbba23
Mert Altuntaş <mertfozzy@gmail.com>
2022-07-24 11:50:11 +0300

    translation works

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
 import React from 'react';
 import { Button, Text } from 'react-native';
 import { StyleSheet, SafeAreaView } from 'react-native';
-import { useEffect } from 'react';
+
 const axios = require('axios').default;
 const { useState, Fragment } = require("react");
 const { StatusBar, ScrollView, View, TextInput } = require("react-native");
-
+const { v4: uuidv4 } = require('uuid');
 
 
 
 const App = () => {
 
-  const [to, setTo] = useState('');
-  const [from, setFrom] = useState('');
-  const [options, setOptions] = useState('');
   const [inputText, setText] = useState('');
   const [responseText, setResponse] = useState('');
 
-  console.log("mert");
+  let key = "6dc546ea8df4448d87b5a2593bed48be";
+  let endpoint = "https://api.cognitive.microsofttranslator.com";
+  let location = "eastus";
+
+ 
   postTranslateService();
 
   function postTranslateService(text) {
 
-    useEffect(()=>{
-      axios.get('https://libretranslate.com/languages',
-      {headers:{'accept':'application/json'}})
-      .then(res=>{
-        console.log(res.data);
-        setOptions(res.data);
-      })
+    axios({
+      baseURL: endpoint,
+      url: '/translate',
+      method: 'post',
+      headers: {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+      },
+      params: {
+        'api-version': '3.0',
+        'from': 'en',
+        'to': ['tr']
+      },
+      data: [{
+        'text': text
+      }],
+      responseType: 'json'
+    }).then(function(response){
+      console.log(JSON.stringify(response.data, null, 4));
+      setResponse(JSON.stringify(response.data, null, 4));
     })
-  
-    //curl - X GET "https://libretranslate.com/languages" - H  "accept: application/json"
 
 
-
-/*     fetch('https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20200418T170345Z.bc1a7bbbbe797646.b3a4b85d9ec55c9b821be6462380cde319272af8&text=' + text) //api url
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res.text)
-        setResponse(res.text)
-      })
-      .catch((error) => {
-        console.log(error)
-      }); */
 
   };
 
